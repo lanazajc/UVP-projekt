b31ab94832cc6f2e825db730134ed3582552c748
Lana Zajc <lana.zajc@gmail.com>
2019-07-09 19:59:54 +0200

    Zasnovala glavne funkcije

--- projekt.py
+++ projekt.py
@@ -1,5 +1,54 @@
-#Začetek projekta?
-#Druga vrstica
-#Trtji poskus
+# SNAKE GAME
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
 
-#Nov poskus
+class cube(object):
+    rows = 0
+    w = 0
+    def __init__(self, start, dirnx=1, dirny=0, color=(255, 0, 0)):
+        pass
+    
+    def move(self, dirnx, dirny):
+        pass
+
+    def draw(self, surface):
+        pass
+
+class snake(object):
+    
+    def __init__(self, color, pos):
+        pass
+
+    def move(self):
+        pass
+
+    def reset(self, pos):
+        pass
+
+    def addCube(self):
+        pass
+
+    def draw(self, surface):
+        pass
+
+    def draw_grid(w, rows, surface):
+        pass
+
+def redraw_window(surface):
+    pass
+
+def random_snack(rows, items):
+    pass
+
+def message_box(subject, content):
+    pass
+
+def main():
+    width = 500
+    height = 500
+    rows = 20
+    win = pygame.display.set_mode((width, height))
+
