0415310ecb8bd645a9bf06c0a009c9a5b8608d3d
Lana Zajc <lana.zajc@gmail.com>
2019-08-11 18:42:35 +0200

    Ločeno pozdravno okno od glavnega

---
+++ Ideje.py
@@ -0,0 +1,27 @@
+Ideje
+
+Message_box Tkinter:
+def prvo_obvestilo(naslov, vsebina):
+    okence = tkinter.Tk()
+    okence.withdraw()
+    messagebox.showinfo("Pozdravljeni!", "Kviz se začenja!")
+    okence.mainloop()
+
+Ideja celotne igre: 
+
+def igra():
+    tocke = 0
+    odgovorjenih = 0
+    while odgovorjenih < 7:
+        for vprasanje iz random uprasanje: 
+            prikazi_vprasanje(vprasanja)
+            for event in mousedown bla bla:
+                ce gumb zadane okno 
+                odgovorjenih += 1
+                preveri_odgovor()
+                if odgovor je true:
+                        tocke += 1
+    rezultat()
+
+def rezultat():
+    zacetni_pozdrav("Čestitamo! Dosegli ste {} točk".format(tocke))
--- Projekt_Kviz.py
+++ Projekt_Kviz.py
@@ -11,12 +11,9 @@ import time
 sirina = 800
 visina = 600
 
-    
 zaslon = pygame.display.set_mode((sirina, visina))
 pygame.display.set_caption("Kviz Matematičnih 10")
-    
-    
-
+clock = pygame.time.Clock
 
 def zacetni_meni():
     prikaz = True
@@ -28,65 +25,72 @@ def zacetni_meni():
         zaslon.fill((0, 255, 0))
         pygame.display.update()
 
-def prvo_obvestilo(naslov, vsebina):
-    okence = tkinter.Tk()
-    okence.withdraw()
-    messagebox.showinfo("Pozdravljeni!", "Kviz se začenja!")
-    okence.mainloop()
-
-
-
-
-
-
 def igra():
+    
     tocke = 0
     odgovorjenih = 0
-    while odgovorjenih < 7:
-        for vprasanje iz random uprasanje: 
-            prikazi_vprasanje(vprasanja)
-            for event in mousedown bla bla:
-                ce gumb zadane okno 
-                odgovorjenih += 1
-                preveri_odgovor()
-                if odgovor je true:
-                        tocke += 1
-    rezultat()
+    while odgovorjenih < 3:
+        prikazi_vprasanje("Koliko je ura?")
+        odgovorjenih += 1
+        tocke += 1
+    rezultat(tocke)
 
-def rezultat():
-        zacetni_pozdrav("Čestitamo! Dosegli ste {} točk".format(tocke))
+def rezultat(tocke):
+        prikazi_vprasanje("Čestitamo! Dosegli ste {} točk".format(tocke))
 
+def besedilo_povrsina_kvadrat_vprasanja(tekst, pisava):
+    povrsina_besedila = pisava.render(tekst, True, (0,255,127))
+    return povrsina_besedila, povrsina_besedila.get_rect()
 
+def prikazi_vprasanje(vprasanja):
+    pisava = pygame.font.Font('freesansbold.ttf', 30)
+    besedilo_povrsina, besedilo_kvadrat = besedilo_povrsina_kvadrat_vprasanja(vprasanja, pisava)
+    besedilo_kvadrat.center = ((sirina / 2), (visina / 3))
+    zaslon.blit(besedilo_povrsina, besedilo_kvadrat)
+    pygame.display.update()
 
-
-    #po tem ko zapres prvo okence
-
-    def prikazi_vprasanje(vprasanja):
-        pisava = pygame.font.Font('freesansbold.ttf', 30)
-        besedilo_povrsina, besedilo_kvadrat = besedilo_povrsina_kvadrat(tekst, pisava)
-        besedilo_kvadrat.center = ((sirina / 2), (visina / 3))
-        zaslon.blit(besedilo_povrsina, besedilo_kvadrat)
-        pygame.display.update()
+    pygame.time.delay(5000)
     
+    zaslon.fill((255, 255, 255))
+    
+
+
+
+
+
+
+
+
+
+
 
 def preveri_odgovor():
     pass
     #preveri ogovor, zabelezi score in zabelezi vprasanje
 
 
-def besedilo_povrsina_kvadrat(tekst, pisava):
+
+def besedilo_povrsina_kvadrat_pozdrav(tekst, pisava):
     povrsina_besedila = pisava.render(tekst, True, (255,255,255))
     return povrsina_besedila, povrsina_besedila.get_rect()
 
 
 def zacetni_pozdrav(tekst):
     pisava = pygame.font.Font('freesansbold.ttf', 30)
-    besedilo_povrsina, besedilo_kvadrat = besedilo_povrsina_kvadrat(tekst, pisava)
+    besedilo_povrsina, besedilo_kvadrat = besedilo_povrsina_kvadrat_pozdrav(tekst, pisava)
     besedilo_kvadrat.center = ((sirina / 2), (visina / 3))
     zaslon.blit(besedilo_povrsina, besedilo_kvadrat)
     pygame.display.update()
-    time.sleep(7)
-    igra()
+    
+    pygame.time.delay(5000)
+
+    zaslon.fill((255, 255, 255))
+    
+
+
+def prazno_okno():
+    zaslon.fill((0,0,0))
+
 
 
 def main():
@@ -104,8 +108,11 @@ def main():
         for event in pygame.event.get():
              if event.type == pygame.QUIT:
                 aktivno = False
-        zacetni_pozdrav("Pripravljeni...")
+        zacetni_pozdrav("Juhu")
         igra()
+        
+        
+        
 
         
 
