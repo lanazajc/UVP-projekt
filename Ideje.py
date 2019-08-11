5472bd9d703b5e088bd3782eada14bfc749f948f
Lana Zajc <lana.zajc@gmail.com>
2019-08-11 21:24:21 +0200

    Ideja

--- Ideje.py
+++ Ideje.py
@@ -25,11 +25,15 @@ def igra():
                         tocke += 1
     rezultat()
 
-def rezultat():
-    zacetni_pozdrav("Čestitamo! Dosegli ste {} točk".format(tocke))
+#### Začetni meni deluje ####
 
+Na začetku uporabnik izbere Začni ali Izhod 
+Nato sledijo vprašanja: 
+X vprašanj, z možnim odgovorom Da / NE ali pa možnostjo Nasledno vprašanje. 
+To je enkratni bonus, torej lahko igrlec eno vprašanje zamenja. 
 
-    if x < pozicija_miske[0] < x + sirina_tipke and y < pozicija_miske[1] < y + visina_tipke:
-        pygame.draw.rect(zaslon, barva, (x, y, sirina_tipke, visina_tipke))
-        if klik[0] == 1 and dogodek != None:
-            dogodek()
+Vprašanja bodo dana v slovarju, ključ bo vprsšanje, vrednost pa Da / ne 
+za vsak klik z misko program preveri na katerem gumbu se je kliknilo in nato naredi primerno akcijo. 
+Ce kliknes na gumb naslednje vprasanje a si bonus ze izkoristil, se izpise Erroor tkinter sporocilo.
+
+Na koncu, po zadnjem (X) vprasanju se izpisejo tocke. 
